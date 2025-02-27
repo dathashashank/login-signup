90626695f0c5a01b4c9fcae76aa24a1a28fa4175
Datha Shashank <dathashashank2000@gmail.com>
2025-02-27 18:52:36 +0530

    login/signup css added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
+    {path:'',component:SignupComponent},
     {path:'login',component:LoginComponent},
     {path:'signup',component:SignupComponent}
 ];
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,6 +7,56 @@ import { Component } from '@angular/core';
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class SignupComponent {
 
+export class SignupComponent {
+  constructor(
+    private router:Router
+  ){}
+
+  loginnav(){
+    this.router.navigate(['/login'])
+  }
 }
+
+// function loginFormvalidtate(){
+//   const email = document.getElementById("loginemail").value;
+//   const password = document.getElementById("loginpassword").value;
+
+//   const emailEr = document.getElementById("lemail-error");
+//   const passEr = document.getElementById("lpassword-error");
+
+//   emailEr.textContent = "";
+//   passEr.textContent = "";
+
+//   let validatelogindetails= true;
+
+//   if (email === "example@gmail.com"){
+//   validatelogindetails= true;
+//   }
+//   else{
+//       emailEr.textContent = "please enter the correct email id";
+//       validatelogindetails = false;
+//   }
+
+//   if(password === "123456"){
+//       validatelogindetails = true;
+//   }
+//   else{
+//       passEr.textContent = "Invalid Password";
+//       validatelogindetails = false;
+//   }
+
+//   if(validatelogindetails){
+//       alert("you have logged in successfully");
+//       resetlogindetails();
+//       return true;
+//   }
+//   else{
+//       return false;
+//   }
+// }
+
+// function resetlogindetails(){
+//   document.getElementById("loginemail").value = "";
+//   document.getElementById("loginpassword").value = "";
+// }
